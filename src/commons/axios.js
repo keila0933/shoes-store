a155371b7f30ed402fd437649e4fcc3a8b36da6a
keila0933 <keila0933@gmail.com>
2021-04-12 16:02:36 +0800

    test heroku

--- src/commons/axios.js
+++ src/commons/axios.js
@@ -2,7 +2,8 @@ import _axios from 'axios'
 
 const axios = (baseURL) => {
   const instance = _axios.create({
-    baseURL: baseURL || 'http://localhost:3004', //if have url parameters,then use it,otherwise using 3004port
+    baseURL:
+      baseURL || process.env.REACT_APP_API_DOMAIN || 'http://localhost:3004', //if have url parameters,then use it,otherwise using 3004port
     timeout: 1000,
   })
 
