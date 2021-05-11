7bdf13ff21e96d228b92270f2fc4a24ca328643a
keila0933 <keila0933@gmail.com>
2021-05-11 15:45:01 +0800

    update axios

--- src/commons/axios.js
+++ src/commons/axios.js
@@ -3,7 +3,9 @@ import _axios from 'axios'
 const axios = (baseURL) => {
   const instance = _axios.create({
     baseURL:
-      baseURL || process.env.REACT_APP_API_DOMAIN || 'http://localhost:3004',
+      baseURL ||
+      'https://vast-dusk-10802.herokuapp.com/' ||
+      'http://localhost:3004',
     //baseURL || process.env.REACT_APP_API_DOMAIN || 'http://localhost:3004', //if have url parameters,then use it,otherwise using 3004port
     timeout: 2000,
   })
